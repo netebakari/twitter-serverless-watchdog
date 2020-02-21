012c6d52a483bb2eb7ccff7d557be005934b5d76
netebakari <itsumo.itsumo.nemui@gmail.com>
2020-02-21 23:39:15 +0900

    ESLint/prettierでフォーマット・修正

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,85 @@
-import * as AWS from "aws-sdk"
-import * as LambdaType from 'aws-lambda'
-import * as Types from "./types"
-import * as Config from "./config"
-import TwitterClient from "./twitterClient"
-import ConfigOnDynamoDb from "./configOnDynamoDb"
+import * as AWS from "aws-sdk";
+import * as LambdaType from "aws-lambda";
+import * as Types from "./types";
+import * as Config from "./config";
+import TwitterClient from "./twitterClient";
+import ConfigOnDynamoDb from "./configOnDynamoDb";
 
-const dynamo = new AWS.DynamoDB.DocumentClient({region: Config.dynamoDb.region});
+const dynamo = new AWS.DynamoDB.DocumentClient({ region: Config.dynamoDb.region });
 const s3 = new AWS.S3();
 
-const updateS3text = async(text) => {
-    const params = {
-        Body: text, 
-        Bucket: Config.s3.bucket, 
-        Key: Config.s3.keyName,
-        ContentType: "text/plain; charset=utf-8",
-        ACL: 'public-read-write'
-    };
-    await s3.putObject(params).promise();
-}
-
+const updateS3text = async (text: string) => {
+  const params = {
+    Body: text,
+    Bucket: Config.s3.bucket,
+    Key: Config.s3.keyName,
+    ContentType: "text/plain; charset=utf-8",
+    ACL: "public-read-write"
+  };
+  await s3.putObject(params).promise();
+};
 
 /**
  * エントリーポイント
  */
 exports.handler = async (event: any, context: LambdaType.Context) => {
-    // Twitterクライアント（のラッパー）を生成。引数で dryRun にtrueが指定されていたらツイート送信は行わない
-    const twitterClient = new TwitterClient(Config.twitterToken, event.dryRun);
+  // Twitterクライアント（のラッパー）を生成。引数で dryRun にtrueが指定されていたらツイート送信は行わない
+  const twitterClient = new TwitterClient(Config.twitterToken, event.dryRun);
 
-    // DynamoDB上の設定を読み書きするクラス。引数で dryRun にtrueが指定されていたら更新は行わない
-    const configOnDynamoDb = new ConfigOnDynamoDb(event.dryRun);
+  // DynamoDB上の設定を読み書きするクラス。引数で dryRun にtrueが指定されていたら更新は行わない
+  const configOnDynamoDb = new ConfigOnDynamoDb(event.dryRun);
 
-    const configOnDb = await configOnDynamoDb.getConfig();
-    console.log("設定を取得しました");
-    console.log(configOnDb);
+  const configOnDb = await configOnDynamoDb.getConfig();
+  console.log("設定を取得しました");
+  console.log(configOnDb);
 
-    if (event.dryRun) {
-        console.log("S3の更新はスキップします");
-    } else {
-        console.log("検索キーワードを列挙したテキストファイルを更新します");
-        await updateS3text(configOnDb.keywords.join("\r\n"));
-    }
+  if (event.dryRun) {
+    console.log("S3の更新はスキップします");
+  } else {
+    console.log("検索キーワードを列挙したテキストファイルを更新します");
+    await updateS3text(configOnDb.keywords.join("\r\n"));
+  }
 
-    // 見つかったツイートとその理由をここに入れる
-    const founds: {tweet: Types.Tweet; keyword: string}[] = [];
-    let maxId = configOnDb.lastId;
+  // 見つかったツイートとその理由をここに入れる
+  const founds: { tweet: Types.Tweet; keyword: string }[] = [];
+  let maxId = configOnDb.lastId;
 
-    for(let screenName of configOnDb.screenNames) {
-        // DynamoDB上の設定で指定されたアカウントごとにツイートを適当に取得
-        const tweets = await twitterClient.getRecentTweets(screenName, Config.tweetsCountToRetrieve, configOnDb.lastId);
-        console.log(`${configOnDb.lastId}より後のツイートを${tweets.length}件取得`);
-        for(let tweet of tweets) {
-            if (maxId < tweet.id_str && maxId.length <= tweet.id_str.length) { maxId = tweet.id_str; }
-            for(let keyword of configOnDb.keywords) {
-                let pattern = new RegExp(keyword);
-                if (tweet.full_text.match(pattern)) {
-                    founds.push({tweet, keyword})
-                    break;
-                }
-            }
+  for (const screenName of configOnDb.screenNames) {
+    // DynamoDB上の設定で指定されたアカウントごとにツイートを適当に取得
+    const tweets = await twitterClient.getRecentTweets(screenName, Config.tweetsCountToRetrieve, configOnDb.lastId);
+    console.log(`${configOnDb.lastId}より後のツイートを${tweets.length}件取得`);
+    for (const tweet of tweets) {
+      if (maxId < tweet.id_str && maxId.length <= tweet.id_str.length) {
+        maxId = tweet.id_str;
+      }
+      for (const keyword of configOnDb.keywords) {
+        const pattern = new RegExp(keyword);
+        if (tweet.full_text.match(pattern)) {
+          founds.push({ tweet, keyword });
+          break;
         }
+      }
     }
-    console.log(`更新後のsinceId: ${maxId}`);
+  }
+  console.log(`更新後のsinceId: ${maxId}`);
 
-    if (founds.length > 0) {
-        const accounts = configOnDb.screenNames.map(x => `@${x}`).join(", ");
-        const text = [
-            `${configOnDb.lastId}以降のチェックを行い、${founds.length}件の監視対象ツイートが見つかりました。 @null宛で引用RTします。`,
-            `現在の監視アカウント: ${accounts}`,
-            `現在の監視キーワード: https://${Config.s3.region}.amazonaws.com/${Config.s3.bucket}/${Config.s3.keyName}`].join("\n");
-        await twitterClient.sendTweet(text);
+  if (founds.length > 0) {
+    const accounts = configOnDb.screenNames.map(x => `@${x}`).join(", ");
+    const text = [
+      `${configOnDb.lastId}以降のチェックを行い、${founds.length}件の監視対象ツイートが見つかりました。 @null宛で引用RTします。`,
+      `現在の監視アカウント: ${accounts}`,
+      `現在の監視キーワード: https://${Config.s3.region}.amazonaws.com/${Config.s3.bucket}/${Config.s3.keyName}`
+    ].join("\n");
+    await twitterClient.sendTweet(text);
 
-        for (let found of founds) {
-            await twitterClient.sendTweet(`@null 理由: "${found.keyword}"にマッチしました\n${TwitterClient.tweetToStatusUrl(found.tweet)}`);
-        }
+    for (const found of founds) {
+      await twitterClient.sendTweet(
+        `@null 理由: "${found.keyword}"にマッチしました\n${TwitterClient.tweetToStatusUrl(found.tweet)}`
+      );
     }
+  }
 
-    // 最終ステータスID更新
-    await configOnDynamoDb.updateSinceId(maxId);
-    return true;
+  // 最終ステータスID更新
+  await configOnDynamoDb.updateSinceId(maxId);
+  return true;
 };
-
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,35 +1,35 @@
 export declare interface ConfigRecordType {
-    Provider: string;
-    lastId: string;
-    screenNames: string[];
-    keywords: string[];
+  Provider: string;
+  lastId: string;
+  screenNames: string[];
+  keywords: string[];
 }
 
 export declare interface Tweet {
-    created_at: string;
-    id: number;
-    id_str: string;
-    full_text: string;
-    truncated: boolean;
-    user: User;
-    is_quote_status: boolean;
-    in_reply_to_status_id?: number;
-    in_reply_to_status_id_str?: string;
-    in_reply_to_user_id?: number;
-    in_reply_to_user_id_str?: string;
-    in_reply_to_screen_name?: string;
+  created_at: string;
+  id: number;
+  id_str: string;
+  full_text: string;
+  truncated: boolean;
+  user: User;
+  is_quote_status: boolean;
+  in_reply_to_status_id?: number;
+  in_reply_to_status_id_str?: string;
+  in_reply_to_user_id?: number;
+  in_reply_to_user_id_str?: string;
+  in_reply_to_screen_name?: string;
 }
 export declare interface User {
-    id: number;
-    id_str: string;
-    name: string;
-    screen_name: string;
-    location: string;
-    description: string;
-    url: string;
-    //entities: [Object],
-    protected: boolean;
-    followers_count: number;
-    friends_count: number;
-    listed_count: number;
+  id: number;
+  id_str: string;
+  name: string;
+  screen_name: string;
+  location: string;
+  description: string;
+  url: string;
+  //entities: [Object],
+  protected: boolean;
+  followers_count: number;
+  friends_count: number;
+  listed_count: number;
 }
